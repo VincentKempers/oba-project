c5eea6851d2d58b08ee219f1a3b45b6bb898ae24
VincentKempers <vincent.m.kempers@gmail.com>
2018-03-07 14:45:25 +0100

    added styling and what simple layouting

--- index.js
+++ index.js
@@ -4,18 +4,19 @@
 
 let app = {
 sparqlquery: `
-    PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
-    SELECT ?cho ?title ?img ?date WHERE {
-     ?cho dc:type "Poster."^^xsd:string .
-     ?cho dc:subject "Music."^^xsd:string .
-     ?cho dc:title ?title .
-     ?cho foaf:depiction ?img .
-     ?cho sem:hasBeginTimeStamp ?date .
-   }
-   ORDER BY ?date
-    LIMIT 100`,
+   PREFIX dc: <http://purl.org/dc/elements/1.1/>
+   PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+   PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
+   SELECT DISTINCT  ?cho ?img ?date ?title WHERE {
+    ?cho dc:type "Poster."^^xsd:string .
+    ?cho dc:subject "Music."^^xsd:string .
+    ?cho dc:title ?title .
+    ?cho foaf:depiction ?img .
+    ?cho sem:hasBeginTimeStamp ?date .
+  FILTER (?title != "[Poster.]"^^xsd:string)
+  }
+  ORDER BY ?date
+   LIMIT 100`,
     init:  function() {
        app.encodedquery = encodeURI(this.sparqlquery);
        app.queryurl= 'https://api.data.adamlink.nl/datasets/AdamNet/all/services/endpoint/sparql?default-graph-uri=&query=' + this.encodedquery + '&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on';
@@ -26,8 +27,13 @@ sparqlquery: `
     
       var rows = data.results.bindings; // get the results
       var imgdiv = document.getElementById('images');
+      console.log(rows);
 
       content.collection = rows.map(function (d) {
+        console.log(d);
+        if (d.title.value == "[Poster.]") {
+          d.title.value = 'onbekend';
+        }
         return {
           image: d.img.value,
           title: d.title.value,
@@ -62,15 +68,6 @@ sparqlquery: `
 
 let content = (function() {
 
-  // function hideElements(selector) {
-  //   document.querySelectorAll(selector).forEach(function(element) {
-  //     element.classList.add('hidden');
-  //   });
-  // }
-  // function showElement(selector) {
-  //     document.querySelector(selector).classList.remove('hidden');
-  // };
-
   var collection = {};
 
   return {
@@ -81,8 +78,8 @@ let content = (function() {
         document.getElementById('detail').classList.add('hidden');
       },
       'detail/*': function(detail) {
-        console.log(detail)
         renderPage.detailIMG(detail);
+        renderPage.detailTekst(detail);
         document.getElementById('detail').classList.remove('hidden');
         document.getElementById('images').classList.add('hidden');
       }
@@ -94,30 +91,33 @@ let content = (function() {
 
 var renderPage = {
   detailIMG: function(detail) {
-    var html = `<div>`;
+    var html;
     content.collection.forEach(function(d) {
       if (detail == d.title) {
         html += `
-        <img src="${d.image}" title="${d.title}">
-        <p>Evenement werd gehouden rond: ${d.date}</p>
-        `;
-      } else {
-        `Couldn't find a poster for this one event`
+        <article>
+          <h3>${d.title}</h3>
+          <img src="${d.image}" title="${d.title}">
+          <p>Evenement werd gehouden rond: ${d.date}</p>
+        </article>`;
+
       }
-      html += `</div>`;
-      document.getElementById("detail").innerHTML = html;
+      document.getElementById("allimages").innerHTML = html;
     });
   }, 
   detailTekst: function(detail){
     var html = `
-        <p>Lorem ipsum dolor sit amet, in amet omnesque pri, vis adhuc imperdiet ei. Te sit nobis nominati reprimique, vis verterem scribentur eu. Ad dicunt molestie partiendo has, an vocent splendide mea. Sapientem abhorreant ei eam. Nam dicta errem appetere in, euismod veritus mnesarchum his ei.
-        </p>
-        <iframe src="https://open.spotify.com/embed?uri=spotify:album:"
-        width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    `
+      <div class="explain">
+          <p>Lorem ipsum dolor sit amet, in amet omnesque pri, vis adhuc imperdiet ei. Te sit nobis nominati reprimique, vis verterem scribentur eu. Ad dicunt molestie partiendo has, an vocent splendide mea. Sapientem abhorreant ei eam. Nam dicta errem appetere in, euismod veritus mnesarchum his ei.
+          </p>
+          <iframe src="https://open.spotify.com/embed?uri=spotify:album:6LtrEATr18sclojAIJ47Bh"
+          width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
+    `;
+    var detailEl = document.getElementById("detail");
+    detailEl.insertAdjacentHTML('beforeend', html);
   }
 };
-
 app.init();
 content.router('images');
 console.log(content.collection);
