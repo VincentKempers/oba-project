f7fbb191cd2a5c23e9ba936aa71d10c04ebee760
VincentKempers <vincent.m.kempers@gmail.com>
2018-03-07 12:19:07 +0100

    fixed the html

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 'use strict';
 
 
-  let app = {
-  sparqlquery: `
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
+let app = {
+sparqlquery: `
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
     PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
     SELECT ?cho ?title ?img ?date WHERE {
@@ -21,22 +21,22 @@ PREFIX dc: <http://purl.org/dc/elements/1.1/>
        app.queryurl= 'https://api.data.adamlink.nl/datasets/AdamNet/all/services/endpoint/sparql?default-graph-uri=&query=' + this.encodedquery + '&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on';
  
       fetch(this.queryurl)
-  .then((resp) => resp.json()) // transform the data into json
-    .then(function(data) {
+      .then((resp) => resp.json()) // transform the data into json
+      .then(function(data) {
     
-    var rows = data.results.bindings; // get the results
-    var imgdiv = document.getElementById('images');
+      var rows = data.results.bindings; // get the results
+      var imgdiv = document.getElementById('images');
 
-    content.collection = rows.map(function (d) {
-      return {
-        image: d.img.value,
-        title: d.title.value,
-        slug: d.title.value.replace(/\s+/g, '-').toLowerCase(),
-        date: d.date.value
-      };
-    });
+      content.collection = rows.map(function (d) {
+        return {
+          image: d.img.value,
+          title: d.title.value,
+          slug: d.title.value.replace(/\s+/g, '-').toLowerCase(),
+          date: d.date.value
+        };
+      });
 
-    for (let i = 0; i < rows.length; ++i) {
+      for (let i = 0; i < rows.length; ++i) {
 
         var sections = document.createElement('div');
         var linkAround = document.createElement('a');
@@ -52,38 +52,39 @@ PREFIX dc: <http://purl.org/dc/elements/1.1/>
         sections.appendChild(linkAround)
         linkAround.appendChild(img);
         linkAround.appendChild(p);
-    }
-  }).catch(function(error) {
-    console.log(error);
+      }
+    })
+      .catch(function(error) {
+        console.log(error);
     });
   }
 };
 
 let content = (function() {
 
-  function hideElements(selector) {
-    document.querySelectorAll(selector).forEach(function(element) {
-      element.classList.add('hidden');
-    });
-  }
-  function showElement(selector) {
-      document.querySelector(selector).classList.remove('hidden');
-  };
+  // function hideElements(selector) {
+  //   document.querySelectorAll(selector).forEach(function(element) {
+  //     element.classList.add('hidden');
+  //   });
+  // }
+  // function showElement(selector) {
+  //     document.querySelector(selector).classList.remove('hidden');
+  // };
 
   var collection = {};
 
   return {
-   toggle: function(id) {
-     hideElements('section');
-     showElement(id);
-   },
    router: function(){
      routie({
       'images': function(){
-        content.toggle(window.location.hash);
+        document.getElementById('images').classList.remove('hidden');
+        document.getElementById('detail').classList.add('hidden');
       },
       'detail/*': function(detail) {
-        renderPage.detailHtml(detail);
+        console.log(detail)
+        renderPage.detailIMG(detail);
+        document.getElementById('detail').classList.remove('hidden');
+        document.getElementById('images').classList.add('hidden');
       }
     });
    },
@@ -92,29 +93,31 @@ let content = (function() {
 })();
 
 var renderPage = {
-  detailHtml: function(detail) {
+  detailIMG: function(detail) {
     var html = `<div>`;
-    content.collection.forEach(function(d){
-      console.log(d);
+    content.collection.forEach(function(d) {
       if (detail == d.title) {
         html += `
-        <h1>${d.title}</h1>
-        <p>${d.date}</p>
         <img src="${d.image}" title="${d.title}">
-        <p>Lorem ipsum dolor sit amet, in amet omnesque pri, vis adhuc imperdiet ei. Te sit nobis nominati reprimique, vis verterem scribentur eu. Ad dicunt molestie partiendo has, an vocent splendide mea. Sapientem abhorreant ei eam. Nam dicta errem appetere in, euismod veritus mnesarchum his ei.
-        </p>
-        <iframe src="https://open.spotify.com/embed?uri=spotify:album:1DFixLWuPkv3KT3TnV35m3"
-        width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <p>Evenement werd gehouden rond: ${d.date}</p>
         `;
       } else {
-        console.log('no');
+        `Couldn't find a poster for this one event`
       }
       html += `</div>`;
       document.getElementById("detail").innerHTML = html;
     });
+  }, 
+  detailTekst: function(detail){
+    var html = `
+        <p>Lorem ipsum dolor sit amet, in amet omnesque pri, vis adhuc imperdiet ei. Te sit nobis nominati reprimique, vis verterem scribentur eu. Ad dicunt molestie partiendo has, an vocent splendide mea. Sapientem abhorreant ei eam. Nam dicta errem appetere in, euismod veritus mnesarchum his ei.
+        </p>
+        <iframe src="https://open.spotify.com/embed?uri=spotify:album:"
+        width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    `
   }
 };
 
 app.init();
-content.router('#images');
+content.router('images');
 console.log(content.collection);
